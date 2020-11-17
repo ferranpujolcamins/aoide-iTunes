e525cb1882d74232d55da8c861f5df649b7651b4
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2020-11-17 16:38:36 +0100

    WIP

---
+++ build.rs
@@ -0,0 +1,35 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to tell rustc to link the system bzip2
+    // shared library.
+    println!("cargo:rustc-link-lib=framework=iTunesLibrary");
+
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let builder = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("wrapper.h")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .clang_args(&["-x", "objective-c"])
+        .objc_extern_crate(true)
+        .rustfmt_bindings(true)
+        .whitelist_type("ITLib.*");
+
+    let bindings = builder
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ wrapper.h
@@ -0,0 +1,1 @@
+#include <iTunesLibrary/iTunesLibrary.h>
